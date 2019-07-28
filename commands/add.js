a8db8a6ed4faf57149e6dea8a0f85e7662552322
dustin pfister <dustin.pfister@gmail.com>
2019-07-28 09:46:25 -0400

    reading json

--- commands/add.js
+++ commands/add.js
@@ -14,8 +14,32 @@ exports.builder = {
 };
 exports.handler = function (argv) {
 
-    //let target = path.resolve(argv.t);
+    if(argv.t && argv.k){
+
+
+        let target = path.resolve(argv.t);
+        
+        fs.readFile(target,'utf8', (e,data) => {
+            
+            if(e){
+                console.log(e.message);
+            }else{
+                try{
+                    let db = JSON.parse(data);
+                    db.keywords.push()
+                }catch(e){
+                    console.log(e.message);
+                }
+            }
+            
+        });
+        
+    }else{
+        
+        console.log('need to give a target path to the database file and a keyword');
+        
+    }
     
-    console.log(argv.t);
-    console.log(argv.k);    
+    //console.log(argv.t);
+    //console.log(argv.k);    
 };
