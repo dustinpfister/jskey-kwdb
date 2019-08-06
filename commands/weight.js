6da1f79a00aefba023ad4e27f17986f5e0987dfb
dustin pfister <dustin.pfister@gmail.com>
2019-08-06 08:27:32 -0400

    weight command started

---
+++ commands/weight.js
@@ -0,0 +1,24 @@
+let path = require('path');
+// lowdb
+let fileAsync = require('lowdb/adapters/FileAsync'),
+low = require('lowdb'); 
+
+exports.command = 'weight';
+exports.describe = 'figure weight for a post';
+exports.builder = {
+    // target database file path to use
+    t: {
+      default: false
+    },
+    // keyword to use
+    k: {
+      default: false
+    }
+};
+exports.handler = function (argv) {
+    console.log('weight command');
+    if(!argv.t && !argv.k){
+        console.warn('must give a single keyword with -k option or a database to use via -t option');
+        console.warn('must also supply text via the standard input.');
+    }
+};
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
 require('yargs')
+
+// default command
 .command(require('./commands/default.js'))
+
+// create and manage a keyword database file
 .command(require('./commands/create.js'))
 .command(require('./commands/drop.js'))
 .command(require('./commands/add.js'))
 .command(require('./commands/remove.js'))
+
+// search and weight
 .command(require('./commands/search.js'))
+.command(require('./commands/weight.js'))
+
 .argv;
