bdb4512dfff823d161c679af3a3a65041e04fa22
dustin pfister <dustin.pfister@gmail.com>
2019-08-11 20:59:59 -0400

    using mkdirp package to create target folder if it is not there

--- commands/create.js
+++ commands/create.js
@@ -1,12 +1,10 @@
-let path = require('path');
+let path = require('path'),
+fs = require('fs'),
+mkdirp = require('mkdirp');
 
 // lowdb
 let fileAsync = require('lowdb/adapters/FileAsync'),
 low = require('lowdb'); 
-
-// fs and mkdirp
-let fs = require('fs'),
-mkdirp = require('../lib/mkdirp.js');
 
 exports.command = 'create';
 exports.describe = 'create database command';
@@ -24,6 +22,10 @@ exports.handler = function (argv) {
 
     // make given target file path absolute
     let filePath = path.resolve(argv.t);
+    
+    // using mkdirp to make sure target folder
+    // is there
+    mkdirp.sync(path.dirname(filePath));
 
     // use the Async adapter
     low(new fileAsync(filePath))
